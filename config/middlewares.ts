3231b1e51db724bde5c0f0a47620f32d2867cb25
josegr15 <jose.raimondi@front.com>
2025-09-08 10:46:09 -0300

    -Added article schema

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000'], // your Next.js app
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+// config/plugins.ts
+export default ({ env }) => ({
+    graphql: {
+      enabled: true,
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        // Sandbox (landing page) is ON by default in non-prod; OFF in prod.
+        // If you *must* enable it in prod (not recommended), set:
+        // landingPage: true,
+      },
+    },
+  });
